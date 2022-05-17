3b97e30f78148b364574534bfa30ba3e716ef40d
Artem Khasin <akhasin.dev@gmail.com>
2022-05-17 17:18:17 +0300

    cart calk

--- src/components/pages/Chekout.js
+++ src/components/pages/Chekout.js
@@ -3,16 +3,45 @@ import { Container } from 'react-bootstrap';
 import ChekoutTable from '../utilites/ChekoutTable';
 import UserInfoForm from '../utilites/UserInfoForm';
 
+//temp data
+const tempData = [
+  {
+    prod_name: 'Some product #1',
+    prod_pic: 'https://picsum.photos/75/50?random=5',
+    prod_price: 33.45
+  },
+  {
+    prod_name: 'Another product #2',
+    prod_pic: 'https://picsum.photos/75/50?random=6',
+    prod_price: 67.89
+  },
+  {
+    prod_name: 'New product #3',
+    prod_pic: 'https://picsum.photos/75/50?random=2',
+    prod_price: 24.99
+  },
+  {
+    prod_name: 'Old product #4',
+    prod_pic: 'https://picsum.photos/75/50?random=3',
+    prod_price: 17.88
+  }
+]
+//temp to del ^^^
+
 const Chekout = () => {
   return (
     <div className="chekout" style={{margin: '2em auto'}}>
       <Container>
         <div className="chekouts">
-          <ChekoutTable/>
-          <ChekoutTable/>
-          <ChekoutTable/>
-          <ChekoutTable/>
-          <h3 className='display-5' style={{textAlign: 'right'}}>Total: <span>34</span>$</h3>
+          {tempData.map((product) => (
+            <ChekoutTable 
+              product={product.prod_name}
+              image={product.prod_pic}
+              price={product.prod_price}
+            />
+          ))}
+          
+          <h3 className='display-6' style={{textAlign: 'right'}}>Total: <span>34</span>$</h3>
         </div>
         <UserInfoForm/>
       </Container>
--- src/components/utilites/ChekoutTable.js
+++ src/components/utilites/ChekoutTable.js
@@ -1,16 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Button } from 'react-bootstrap';
 
-const ChekoutTable = () => {
+const ChekoutTable = ({price, image, product}) => {
+
+  const [itemQuantity, setItemQuantity] = useState(1)
+  // eslint-disable-next-line
+  const [itemCartPrice, setItemCartPrice] = useState(price)
+
   return (
     <div className='checkOutTable' style={{display: 'flex', justifyContent: 'space-between', margin: '1.5em auto', alignItems: 'center'}}>
-        <img src="https://picsum.photos/75/50?random=5" alt="prod pic" />
-        <h5 className='display-6'>Product name</h5>
-        <Button variant="success" size='sm'>-</Button>
-        <span>Quantity: 0</span>
-        <Button variant="success" size='sm'>+</Button>
+        <img src={image} alt="prod pic" />
+        <h5 className='display-7'>{product}</h5>
+        <Button variant="success" size='sm' onClick={() => setItemQuantity(prev => prev -1)}>-</Button>
+        <span>{itemQuantity}</span>
+        <Button variant="success" size='sm' onClick={() => setItemQuantity(prev => prev +1)}>+</Button>
         <Button variant="danger" size='sm'>DELETE</Button>
-        <Button variant="warning" disabled>33$</Button>
+        <Button variant="warning" disabled>{(itemCartPrice * itemQuantity).toFixed(2)}$</Button>
         <hr/>
     </div>
   )
