3eb090e5497629edb532e24a6b50544c9c194f87
Artem Khasin <akhasin.dev@gmail.com>
2022-05-18 16:36:57 +0300

    created fake DB with products

--- src/components/common/ProductCard.js
+++ src/components/common/ProductCard.js
@@ -2,20 +2,27 @@ import React from 'react'
 import { Card, Col, Button } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 
-const ProductCard = ({number, price}) => {
+const ProductCard = ({
+    product_brand, 
+    product_category, 
+    product_description, 
+    product_image, 
+    product_name, 
+    product_price
+}) => {
 
     let navigate = useNavigate();
 
   return (
     <Col>
         <Card style={{ width: '30%', minWidth: '18em', marginTop: '2em' }}>
-            <Card.Img variant="top" src={`https://picsum.photos/200/150?random=${number}`} />
+            <Card.Img variant="top" src={product_image} />
             <Card.Body>
-                <Card.Title>Product {number}</Card.Title>
+                <Card.Title>{product_name}</Card.Title>
                 <Card.Text>
-                    <p>Some quick example text to build on the card title and make up the bulk of
-                    the card's content.</p>
-                    <span>Price: {price}$</span>
+                    <p>{product_brand} {product_description}</p>
+                    <span>Price: {product_price}$</span>
+                    <span>Category: {product_category}$</span>
                 </Card.Text>
                 <Button variant="danger" size="sm" style={{marginRight: '1em'}}>ADD TO CART</Button>
                 <Button variant="outline-secondary" size="sm" onClick={()=>{navigate("/product")}}>VIEW PRODUCT</Button>
--- src/components/common/Store.js
+++ src/components/common/Store.js
@@ -1,22 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container, Row } from 'react-bootstrap';
 import ProductCard from './ProductCard';
 
+import DATA from '../../db/temp_prod_list.json'
+
+
 const Store = () => {
+ 
+  const [ products, setProducts ] = useState(DATA.product_list)
+
   return (
     <div className='store'>
         <Container text-center>
             <h1 className="display-2 text-center">Products</h1>
             <Row>
-                <ProductCard number={1} price={22.99}/>
-                <ProductCard number={2} price={16.88}/>
-                <ProductCard number={3} price={23.65}/>
-                <ProductCard number={4} price={15.55}/>
-                <ProductCard number={5} price={133.99}/>
-                <ProductCard number={6} price={8.99}/> 
-                <ProductCard number={7} price={133.99}/>
-                <ProductCard number={8} price={8.99}/>
-                <ProductCard number={9} price={8.99}/> 
+              {products.map(product => (
+                <ProductCard 
+                  product_brand={product.prod_brand} 
+                  product_category={product.prod_category} 
+                  product_description={product.prod_descr}
+                  product_image={product.prod_pic} 
+                  product_name={product.prod_name}
+                  product_price={product.prod_price}
+
+                />
+              ))}
+
+                
+                
             </Row>
         </Container>
     </div>
