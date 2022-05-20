5ff6fb23ccfd5b7117093ffd1f81bb3bd6077244
Artem Khasin <akhasin.dev@gmail.com>
2022-05-20 16:51:22 +0300

    checkoutTabje.js stuck with delete function

--- src/components/pages/Chekout.js
+++ src/components/pages/Chekout.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState} from 'react';
 import { Button, Container } from 'react-bootstrap';
 import ChekoutTable from '../utilites/ChekoutTable';
 import UserInfoForm from '../utilites/UserInfoForm';
@@ -10,6 +10,16 @@ const Chekout = () => {
   const { itemsInCart } = useContext(MyContext)
   // console.log(itemsInCart)
   const [formIsShown, setFormIsShown] = useState(false)
+
+  function totalCost() {
+    let result = 0
+    for(let i = 0; i < itemsInCart.length; i++ ){
+      result = result + itemsInCart[i].prod_price
+    }
+    return result.toFixed(2)
+  }
+
+ 
 
   return (
     <div className="chekout" style={{margin: '2em auto'}}>
@@ -24,7 +34,7 @@ const Chekout = () => {
             />
           ))}
           
-          {itemsInCart.length > 1 && <h3 className='display-6' style={{textAlign: 'right'}}>Total: <span>0</span>$</h3>}
+          {itemsInCart.length > 1 && <h3 className='display-6' style={{textAlign: 'right'}}>Total: <span>{totalCost()}</span>$</h3>}
         </div>
         {itemsInCart.length > 1 && <Button variant='danger' onClick={() => setFormIsShown(prev => !prev)}>Proceed Chekout</Button>}
         {formIsShown && <UserInfoForm/>}
--- src/components/utilites/ChekoutTable.js
+++ src/components/utilites/ChekoutTable.js
@@ -1,21 +1,28 @@
-import React, { useState } from 'react'
+import React, { useRef, useContext } from 'react'
 import { Button } from 'react-bootstrap';
+import { MyContext } from '../context/configContext';
 
-const ChekoutTable = ({price, image, product_name}) => {
+const ChekoutTable = ({price, image, product_name, handleDeleteItem}) => {
 
-  const [itemQuantity, setItemQuantity] = useState(1)
+  //const [itemQuantity, setItemQuantity] = useState(1)
   // eslint-disable-next-line
-  const [itemCartPrice, setItemCartPrice] = useState(price)
+  //const [itemCartPrice, setItemCartPrice] = useState(price)
+  const { itemsInCart, setItemsInCart } = useContext(MyContext)
+  const cardRef = useRef()
+
+  function handleDeleteItem(){
+    setItemsInCart(prev => [...prev, itemsInCart.filter(product => product.id !== Number(cardRef.current.id))[0]]);
+  }
 
   return (
-    <div className='checkOutTable' style={{display: 'flex', justifyContent: 'space-between', margin: '1.5em auto', alignItems: 'center'}}>
+    <div className='checkOutTable' style={{display: 'flex', justifyContent: 'space-between', margin: '1.5em auto', alignItems: 'center'}} ref={cardRef}>
         <img src={image} alt="prod pic" style={{widt: '100px', height: '50px'}}/>
         <h5 className='display-7'>{product_name}</h5>
-        <Button variant="success" size='sm' onClick={() => setItemQuantity(prev => prev -1)}>-</Button>
+        {/*<Button variant="success" size='sm' onClick={() => setItemQuantity(prev => prev -1)}>-</Button>
         <span>{itemQuantity}</span>
-        <Button variant="success" size='sm' onClick={() => setItemQuantity(prev => prev +1)}>+</Button>
-        <Button variant="danger" size='sm'>DELETE</Button>
-        <Button variant="warning" disabled>{(itemCartPrice * itemQuantity).toFixed(2)}$</Button>
+  <Button variant="success" size='sm' onClick={() => setItemQuantity(prev => prev +1)}>+</Button>*/}
+        <Button variant="danger" size='sm' onClick={(cardRef) => handleDeleteItem()}>DELETE</Button>
+        <Button variant="warning" disabled>{(price).toFixed(2)}$</Button>
         <hr/>
     </div>
   )
